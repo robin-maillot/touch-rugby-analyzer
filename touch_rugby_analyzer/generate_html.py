37fc755d9349767c798afa9514597dd869cc58a6
robin <robin@safi.co>
2025-09-17 22:58:17 +0200

    Update template to separate stats

--- touch_rugby_analyzer/generate_html.py
+++ touch_rugby_analyzer/generate_html.py
@@ -1,17 +1,33 @@
-import plotly.express as px
+import pandas as pd
 from jinja2 import Template
-from touch_rugby_analyzer.constants import ASSETS_ROOT, ROOT
 
-data_canada = px.data.gapminder().query("country == 'Canada'")
-fig = px.bar(data_canada, x="year", y="pop")
-
+from touch_rugby_analyzer.constants import ASSETS_ROOT, ROOT, DATA_ROOT
+from touch_rugby_analyzer import utils
 input_template_path = ASSETS_ROOT / "template.html"
-output_html_path = ROOT / "index.html"
+for i, data_path in enumerate(
+    [
+        DATA_ROOT / "france_pays-bas.csv",
+        DATA_ROOT / "france_england.csv",
+    ]
+):
+    output_html_path = ROOT / f"game_{i}.html"
+    local_team_name, other_team_name = utils.get_names(data_path)
+    data_df = utils.load_data(data_path, local_team_name, other_team_name)
 
-plotly_jinja_data = {"fig": fig.to_html(full_html=False)}
-# consider also defining the include_plotlyjs parameter to point to an external Plotly.js as described above
+    stats_dict = utils.get_stats_df(data_df, local_team_name, other_team_name)
+    trend_fig = utils.make_fig_1(data_df, local_team_name, other_team_name)
+    game_fig = utils.make_game_fig(data_df, local_team_name, other_team_name)
 
-with output_html_path.open("w", encoding="utf-8") as output_file:
-    with input_template_path.open() as template_file:
-        j2_template = Template(template_file.read())
-        output_file.write(j2_template.render(plotly_jinja_data))
+    plotly_jinja_data = {
+        "game_fig": game_fig.to_html(full_html=False),
+        "trend_fig": trend_fig.to_html(full_html=False),
+        "stats_tries_table": stats_dict.get("Try", pd.DataFrame()).to_html(),
+        "stats_penalties_table": stats_dict.get("Penalty", pd.DataFrame()).to_html(),
+        "stats_turnovers_table": stats_dict.get("Turnover", pd.DataFrame()).to_html(),
+    }
+    # consider also defining the include_plotlyjs parameter to point to an external Plotly.js as described above
+
+    with output_html_path.open("w", encoding="utf-8") as output_file:
+        with input_template_path.open() as template_file:
+            j2_template = Template(template_file.read())
+            output_file.write(j2_template.render(plotly_jinja_data))
